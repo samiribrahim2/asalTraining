db0cd020fa9c51527179934f64b75d5f38c96513
samiribrahim2 <samirnasim901@gmail.com>
2025-11-13 19:02:53 +0200

    updates on mobile menu

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('menu-icon').addEventListener('click' , () =>{
     console.log("clicked");
     document.querySelector('.mobile-menu').classList.toggle('hidden')
 });
-document.getElementById('close-menu').addEventListener('click' , () =>{
+document.querySelector('.mobile-menu').addEventListener('click' , () =>{
     console.log("clicked");
     document.querySelector('.mobile-menu').classList.toggle('hidden')
 });
